095553d3c64e5e1b8f4017669f995d144c766d0f
thaynna cabral <tertothaynna@gmail.com>
2023-02-25 18:03:05 +0100

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  important: true,
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
